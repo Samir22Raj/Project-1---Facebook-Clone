010007f36a37215e16694c0104d60ab0d96c1b6c
Samir Rajkarnikar <samirrajkarnikar9@gmail.com>
2024-02-27 13:15:28 +0545

    fb clone

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["*"],
+	theme: {
+		extend: {
+			colors: {
+				blue: {
+					org: "#1877f2",
+					hov: "#4267b2",
+				},
+				green: {
+					org: "#42b72a",
+					hov: "#00a400",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
